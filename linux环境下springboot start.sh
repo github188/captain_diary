9e5cc1da0b8db44840dfaede352b6eea753ff3ad
USA <huangleisir@qq.com>
2017-08-09 08:54:27 +0800

    Update linux环境下springboot start.sh

--- linux环境下springboot start.sh
+++ linux环境下springboot start.sh
@@ -1,2 +1,6 @@
 linux环境下 start.sh 
 nohup java -jar -Dspring.profiles.active=dev -Xms1024m -Xmx1024m -XX:PermSize=256M -XX:MaxPermSize=512m member.jar 2>&1 & 
+nohup是在后台执行该程序 以守护线程的形式  前台退出该线程  后台该线程仍然在跑
+启完之后  出现这个不要犯傻 nohup: ignoring input and appending output to `nohup.out'
+赶紧去 tail -f nohup.out 看启动日志就行了 
+
